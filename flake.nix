7f0048b3bca8fbfe51c01dbdbf1a40e93529c276
Knut Magnus Aasrud <km@aasrud.com>
2024-06-27 21:18:52 +0200

    build: reset to basic flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    nixpkgs,
-    crane,
-    fenix,
-    ...
-  }: let
+  outputs = {nixpkgs, ...}: let
     eachSystem = systems: f: let
       # Merge together the outputs for all systems.
       op = attrs: system: let
@@ -38,42 +23,10 @@
     forAllSystems = eachSystem systems;
   in
     forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      crossPkgs = import nixpkgs {
-        inherit system;
-        crossSystem = "armv6l-linux";
-      };
-
-      toolchain = with fenix.packages.${system};
-        combine [
-          stable.rustc
-          stable.cargo
-          stable.clippy
-          stable.rustfmt
-          targets.arm-unknown-linux-musleabihf.stable.rust-std
-        ];
-
-      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
-    in rec {
-      packages.default = craneLib.buildPackage rec {
-        src = pkgs.lib.cleanSourceWith {src = craneLib.path ./.;};
-      };
-
-      apps.default = {
-        drv = packages.default;
-      };
-
+      pkgs = import nixpkgs {inherit system;};
+    in {
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [rust-analyzer toolchain];
-        # CARGO_TARGET_ARM_UNKNOWN_LINUX_MUSLEABIHF_LINKER = "${pkgs.stdenv.cc.targetPrefix}cc";
-      };
-
-      devShells.pizero = crossPkgs.mkShell {
-        nativeBuildInputs = with crossPkgs; [toolchain];
-        CARGO_TARGET_ARM_UNKNOWN_LINUX_MUSLEABIHF_LINKER = "${crossPkgs.stdenv.cc.targetPrefix}cc";
+        nativeBuildInputs = with pkgs; [python3];
       };
     });
 }
